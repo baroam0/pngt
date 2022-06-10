c832d60d7146010f8339827a8e0378e09a30a0d5
Abel Barrios <barriosabel0@hotmail.com>
2022-06-10 12:26:55 -0300

    Cambios en nombre de la rama para actualizar

--- actualizar.ps1
+++ actualizar.ps1
@@ -1,4 +1,4 @@
 
 
-git pull origin master; python manage.py makemigrations --settings=pngt.local_settings; python manage.py migrate --settings=pngt.local_settings
+git pull origin main; python manage.py makemigrations --settings=pngt.local_settings; python manage.py migrate --settings=pngt.local_settings
 
